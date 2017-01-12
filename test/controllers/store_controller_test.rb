dca424787a3983555d9d9527579cce7aaa30a83c
Jeff Sutherland <jdsuther@asu.edu>
2017-01-12 13:30:09 +0700

    Add test to store controller

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -6,4 +6,15 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "displays product with price" do
+    PRICE_REGEX = /\$[,\d]+\.\d\d/
+    product = products(:ruby)
+
+    get store_index_path
+
+    assert_template 'store/index'
+    assert_select 'h3', product.title
+    assert_select '.price', PRICE_REGEX
+  end
+
 end
