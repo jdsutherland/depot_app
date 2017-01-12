62f433a7974eb69ffcbfa74908bdc63bc9657ea7
Jeff Sutherland <jdsuther@asu.edu>
2017-01-12 13:14:05 +0700

    Refactor layout to partials with integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'store#index'
   root 'store#index', as: 'store_index'
 
   resources :products
---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test 'layout links' do
+    get root_path
+    assert_template 'store/index'
+
+    assert_select 'title', full_title('Catalog')
+
+    # TODO: add links
+    assert_select 'a[href=?]', root_path
+    # assert_select 'a[href=?]', help_path
+    # assert_select 'a[href=?]', about_path
+    # assert_select 'a[href=?]', contact_path
+    # get contact_path
+    # assert_select 'title', full_title('Contact')
+    # get signup_path
+    # assert_select 'title', full_title('Sign up')
+  end
+end
